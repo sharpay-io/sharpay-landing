43308f3250fd076b5ea04478eeab542e6ae81ef0
Aleksey <liketux@gmail.com>
2018-01-18 17:30:24 +0300

    stat for sale

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+(function() {
+	// Google Analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','gaSharpay');
+	gaSharpay('create', 'UA-109731082-3', 'auto');
+	gaSharpay('send', 'pageview');
+	
+})();
