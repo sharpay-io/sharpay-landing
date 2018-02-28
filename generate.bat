d3d5e9208870d5f6c28d4a03a38e9122f8ac777c
liketux <liketux@gmail.com>
2018-02-28 22:08:57 +0300

    start Japanese translate

--- generate.bat
+++ generate.bat
@@ -11,5 +11,8 @@ C:\xampp\php\php.exe -f  src/index.php ko > ko/index.new.html
 ECHO Generating russian page...
 C:\xampp\php\php.exe -f src/index.php ru > ru/index.new.html
 
+ECHO Generating japanese page...
+C:\xampp\php\php.exe -f src/index.php jp > jp/index.new.html
+
 ECHO Done.
 pause
