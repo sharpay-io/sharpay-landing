1583306d1408afe3de8fc5c0a9b99e699439b740
liketux <liketux@gmail.com>
2018-02-28 09:01:09 +0300

    win generation script

---
+++ generate.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+ECHO Generating default (english) page...
+C:\xampp\php\php.exe -f  src/index.php en > index.new.html
+
+ECHO Generating chinese page...
+C:\xampp\php\php.exe -f  src/index.php zh > zh/index.new.html
+
+ECHO Generating korian page...
+C:\xampp\php\php.exe -f  src/index.php ko > ko/index.new.html
+
+ECHO Generating russian page...
+C:\xampp\php\php.exe -f src/index.php ru > ru/index.new.html
+
+ECHO Done.
+pause
