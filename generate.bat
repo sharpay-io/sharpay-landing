30d2199d070ce6eca25e30ff2bf59c4abfebb4fb
liketux <liketux@gmail.com>
2018-03-01 01:12:07 +0300

    jp flag add

--- generate.bat
+++ generate.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 ECHO Generating default (english) page...
-C:\xampp\php\php.exe -f  src/index.php en > index.new.html
+C:\xampp\php\php.exe -f  src/index.php en > index.html
 
 ECHO Generating chinese page...
-C:\xampp\php\php.exe -f  src/index.php zh > zh/index.new.html
+C:\xampp\php\php.exe -f  src/index.php zh > zh/index.html
 
 ECHO Generating korian page...
-C:\xampp\php\php.exe -f  src/index.php ko > ko/index.new.html
+C:\xampp\php\php.exe -f  src/index.php ko > ko/index.html
 
 ECHO Generating russian page...
 C:\xampp\php\php.exe -f src/index.php ru > ru/index.new.html
