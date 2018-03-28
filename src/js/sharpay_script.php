732192700039f54eba9db1bf5a99c0d3bfe36b30
liketux <liketux@gmail.com>
2018-03-28 16:56:23 +0300

    fix video analytics

--- src/js/sharpay_script.php
+++ src/js/sharpay_script.php
@@ -60,7 +60,7 @@ $(function() {
         h = w*ratio,
         isChina = $('html').attr('lang') === 'zh';
 
-    var videoHtml = '<iframe width="' + w + '" height="' + h + '" src="https://www.youtube.com/embed/dWIF3bsxbLI?autoplay=1&controls=0&rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    var videoHtml = '<iframe width="' + w + '" height="' + h + '" src="https://www.youtube.com/embed/dWIF3bsxbLI?autoplay=1&rel=0&controls=0&showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 
     if (isChina) {
       videoHtml = '<iframe width="' + w + '" height="' + h + '" src="//rutube.ru/play/embed/10940174?autoStart=true" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>';
