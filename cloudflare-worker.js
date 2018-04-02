1cbfb185ca6c24f9c7e054e0d98b8cd44e891715
liketux <liketux@gmail.com>
2018-04-02 16:56:28 +0300

    By country redirection worker

---
+++ cloudflare-worker.js
@@ -0,0 +1,31 @@
+addEventListener('fetch', event => {
+	event.respondWith(handle(event.request))
+})
+
+async function handle(request) 
+{
+	try {
+		let url = new URL(request.url)
+		if( url.hostname == 'sharpay.io' )
+		{
+			const langs = { 
+				'RU':'ru', 'UA':'ru', 'BY':'ru', 'KZ':'ru', 'CN':'zh', 'KR':'ko', 'KP':'ko', 'JP':'jp', 'IT':'it',
+				'ES':'es', 'PH':'es', 'GQ':'es', 'AR':'es', 'BO':'es', 'CL':'es', 'CO':'es', 'CR':'es', 'CU':'es', 'DO':'es', 'EC':'es', 'GT':'es', 'HN':'es', 'MX':'es', 'NI':'es', 'PA':'es', 'PY':'es', 'PE':'es', 'PR':'es', 'SV':'es', 'UY':'es', 'VE':'es',
+				'PT':'pt', 'AO':'pt', 'BR':'pt', 'TL':'pt', 'GW':'pt', 'CV':'pt', 'MZ':'pt', 'ST':'pt'
+			}
+			let cookies = request.headers.get('Cookie') || null
+			let useragent = request.headers.get('User-Agent') || null
+			if ( ! ( ( useragent && useragent.includes('bot') ) || ( cookies && cookies.includes("language=true") ) ) && url.pathname == "/" ) 
+			{
+				let country = request.headers.get("CF-IpCountry")
+				if( langs[ country ] !== undefined ) 
+				{
+					url.pathname = "/" + langs[ country ] + "/"
+					return Response.redirect(url, 302)
+				}
+			} 
+		}
+	} 
+	catch ( e ) {}	
+	return fetch(request)
+}
