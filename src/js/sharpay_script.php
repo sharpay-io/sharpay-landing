b89b8e258b6641dd0e6a5460a38049f6117e70d2
liketux <liketux@gmail.com>
2018-05-31 08:42:59 +0300

    fix

--- src/js/sharpay_script.php
+++ src/js/sharpay_script.php
@@ -12,24 +12,26 @@
 <script>
 var Webflow = Webflow || [];
 
-Webflow.push(function() {
-  $('.clock').TimeCircles({
-    circle_bg_color: 'rgba(255,255,255,0.05)',
-    fg_width: 0.05,
-    bg_width: 1,
-    time: {
-      Days: { color: "#ff9a1e", text: "days" },
-      Hours: { color: "#ff9a1e", text: "hours" },
-      Minutes: { color: "#ff9a1e", text: "minutes" },
-      Seconds: { color: "#ff9a1e", text: "sec" }
-    }
-  });
-  Webflow.resize.on(function(){
-    $('.clock').TimeCircles().rebuild();
-  });
-});
+
 
 $(function() {
+	
+	Webflow.push(function() {
+	  $('.clock').TimeCircles({
+		circle_bg_color: 'rgba(255,255,255,0.05)',
+		fg_width: 0.05,
+		bg_width: 1,
+		time: {
+		  Days: { color: "#ff9a1e", text: "days" },
+		  Hours: { color: "#ff9a1e", text: "hours" },
+		  Minutes: { color: "#ff9a1e", text: "minutes" },
+		  Seconds: { color: "#ff9a1e", text: "sec" }
+		}
+	  });
+	  Webflow.resize.on(function(){
+		$('.clock').TimeCircles().rebuild();
+	  });
+	});
 	
 	if( /utm/i.test( document.location.search ) ) {
 		$('.btn-sale').attr('href', $('.btn-sale').attr('href') + '&' + document.location.search.substr(1) );
